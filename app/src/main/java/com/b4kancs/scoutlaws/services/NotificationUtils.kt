9dd0a111954d536d7ca7f26866b721e19751ad1f
Szilard Hompoth <hompoth.szilard@gmail.com>
2018-08-25 00:02:05 +0300

    Notifications start quizzes; version++

--- app/src/main/java/com/b4kancs/scoutlaws/services/NotificationUtils.kt
+++ app/src/main/java/com/b4kancs/scoutlaws/services/NotificationUtils.kt
@@ -5,8 +5,16 @@ import android.support.v4.app.NotificationCompat
 import com.b4kancs.scoutlaws.R
 import android.app.NotificationManager
 import android.app.NotificationChannel
+import android.app.PendingIntent
+import android.content.Intent
 import android.os.Build
+import android.preference.PreferenceManager
 import android.support.v4.app.NotificationManagerCompat
+import android.support.v4.app.TaskStackBuilder
+import com.b4kancs.scoutlaws.views.quiz.QuizActivity
+import com.b4kancs.scoutlaws.views.quiz.multiplechoice.MultipleChoiceFragment
+import com.b4kancs.scoutlaws.views.quiz.pickandchoose.PickAndChooseFragment
+import java.util.*
 
 const val CHANNEL_ID = "NOTIFICATION_CHANNEL_1"
 const val NOTIFICATION_ID = 1
@@ -26,10 +34,27 @@ private fun raiseQuizPromptNotification(context: Context): NotificationCompat.Bu
             .setSmallIcon(R.drawable.ic_lily_24dp)
             .setContentTitle(context.getString(R.string.quiz_notification_title))
             .setContentText(context.getString(R.string.quiz_notification_text))
+            .setContentIntent(createQuizIntent(context))
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
             .setAutoCancel(true)
             .setDefaults(NotificationCompat.DEFAULT_LIGHTS)
             .setDefaults(NotificationCompat.DEFAULT_VIBRATE)
+
+private fun createQuizIntent(context: Context) : PendingIntent {
+    val quizIntent = Intent(context, QuizActivity::class.java)
+
+    var type = PreferenceManager.getDefaultSharedPreferences(context).getString("pref_notification_quiz_type", "0")
+    if (type == "0")
+        type = (Random().nextInt() % 2 + 1).toString()
+    when (type) {
+        "1" -> quizIntent.putExtra(QuizActivity.QUIZ_FRAGMENT_EXTRA, MultipleChoiceFragment.FRAGMENT_TAG)
+        "2" -> quizIntent.putExtra(QuizActivity.QUIZ_FRAGMENT_EXTRA, PickAndChooseFragment.FRAGMENT_TAG)
+    }
+
+    val stackBuilder = TaskStackBuilder.create(context)
+    stackBuilder.addNextIntentWithParentStack(quizIntent)
+    return stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT)!!
+}
 
 private fun createNotificationChannel(context: Context) {
     // Create the NotificationChannel, but only on API 26+ because
